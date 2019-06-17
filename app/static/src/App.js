571872e21c6c46bab3ab2e959435e5e4b207b89e
Dmitriy Kel <dmitriykel@MacBook-Pro-Dmitriy.local>
2019-06-17 21:59:00 +0700

    add Home and Auth pages design

--- app/static/src/App.js
+++ app/static/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import './App.css';
+import './App.scss';
 import Routes from "./Routes";
 
 export default class App extends React.Component {
--- app/static/src/Authorization.js
+++ app/static/src/Authorization.js
@@ -1,14 +1,18 @@
 import React from 'react';
-import './App.css'
-import './Authorization.css'
+import './App.scss'
+import './Authorization.scss'
 import logo from "./logo.svg";
+import $ from "jquery";
 
 const axios = require('axios');
 
 export default class Authorization extends React.Component {
     constructor(props) {
         super(props);
-        this.state = {secret: ''};
+        this.state = {
+            secret: '',
+            error: ''
+        };
 
         this.handleChange = this.handleChange.bind(this);
         this.handleSubmit = this.handleSubmit.bind(this);
@@ -21,46 +25,65 @@ export default class Authorization extends React.Component {
 
     handleSubmit(event) {
         event.preventDefault();
-        const test = { 'secret': this.state.secret };
+        const secret_json = { 'secret': this.state.secret };
 
         axios.post(
             "http://127.0.0.1:3000/balder/api/v1.0/check_secret",
-            JSON.stringify(test),
+            JSON.stringify(secret_json),
             {
                 headers: {'Content-Type': 'application/json'}
             }
         )
         .then(
             response => {
-                console.log(response.data['success'])
+                console.log(response.data['success']);
                 this.props.userHasAuthorized(true);
+                this.setState({error: ''});
                 this.props.history.push("/");
             }
         )
         .catch(
             error => {
-                console.log(error)
+                this.setState({error: 'Incorrect secret word!'});
             });
     }
 
-    render() {
+    componentDidMount() {
         if(this.props.isAuthorized) this.props.history.push("/");
+    }
+
+    render() {
         return (
-            <header className="App-header">
-                <div className="Authorizarion">
-                    <p className="head">Назови слово!</p>
-                    <img src={logo} className="App-logo" alt="logo"/>
-                    <form onSubmit={this.handleSubmit} method="POST">
-                        <input
-                            id="secret"
-                            type="text"
-                            value={this.state.secret}
-                            onChange={this.handleChange}
-                        />
-                        <input type="submit" value="Submit" />
+            <div className="auth_container">
+                <div className="profile">
+                    <button className="profile_avatar" id="toggleProfile">
+                        <img src={logo} alt="logo"/>
+                    </button>
+                    <form className="profile_form" onSubmit={this.handleSubmit} method="POST">
+                        <div className="profile_fields">
+                            <div className="field">
+                                <input
+                                    id="secret"
+                                    type="text"
+                                    value={this.state.secret}
+                                    onChange={this.handleChange}
+                                />
+                                <span className="error">{this.state.error}</span>
+                            </div>
+                            <div className="profile_footer">
+                                <input type="submit" value="Enter" />
+                                <span>Hit enter to confirm</span>
+                            </div>
+                        </div>
                     </form>
                 </div>
-            </header>
+            </div>
         );
     }
 }
+
+$(function () {
+   $('#toggleProfile').on('click', function () {
+     $('div.profile').toggleClass('open');
+   })
+});
