7e27790dc2a0d254194a73c896484b63ac5b2f14
Dmitriy Vasilev <d.vasilev@2gis.ru>
2019-06-18 13:15:08 +0700

    create gift table and api upgrade

--- app/routes.py
+++ app/routes.py
@@ -1,27 +1,27 @@
 from flask import request, jsonify, make_response, abort
 from app import app
-from app.models import AuthorizationCodes, Surprises
+from app.models import AuthorizationCodes, Gifts
 
 
 @app.errorhandler(403)
-def not_found(error):
+def not_found():
     return make_response(jsonify({'error': 'Invalid or no secret word.'}), 403)
 
 
 @app.errorhandler(404)
-def not_found(error):
+def not_found():
     return make_response(jsonify({'error': 'Not found.'}), 404)
 
 
 @app.route('/balder/api/v1.0/gifts', methods=['GET'])
 def get_gifts():
     gifts_json = []
-    gifts_list = Surprises.query.all()
+    gifts_list = Gifts.query.all()
     for gift in gifts_list:
         gift_obj = {
             "id": gift.id,
-            "content": gift.content,
-            "released": gift.released
+            "type": gift.type,
+            "data": gift.data
         }
         gifts_json.append(gift_obj)
     return jsonify({"gifts": gifts_json})
@@ -29,14 +29,14 @@ def get_gifts():
 
 @app.route('/balder/api/v1.0/gift/<int:gift_id>', methods=['GET'])
 def get_gift(gift_id):
-    gift = Surprises.query.get(gift_id)
+    gift = Gifts.query.get(gift_id)
     if gift is None:
         abort(404)
     else:
         gift_obj = {
             "id": gift.id,
-            "content": gift.content,
-            "released": gift.released
+            "type": gift.type,
+            "data": gift.data
         }
         return jsonify({"gift": gift_obj})
 
@@ -45,7 +45,7 @@ def get_gift(gift_id):
 def check_secret():
     secret_hash = AuthorizationCodes.query.get(1)
 
-    if not request.json or not 'secret' in request.json:
+    if not request.json or 'secret' not in request.json:
         abort(403)
 
     if not secret_hash.check_secret(request.json['secret']):
