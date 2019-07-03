c710d33c787b7f6c318f1de6bbd0919ceeb971db
Dmitriy Kehl <dmitriykehl@gmail.com>
2019-07-03 20:38:20 +0700

    fix bugs

--- app/src/Home.js
+++ app/src/Home.js
@@ -10,6 +10,53 @@ export default class Home extends Component {
         this.state = {
             gifts: []
         };
+        this.clickHandlerClose = this.clickHandlerClose.bind(this);
+    }
+
+    setOpenDate(gift_id, date_json) {
+        axios.put(
+        "/api/v1.0/" + gift_id + "/open",
+        JSON.stringify(date_json),
+        {
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': localStorage.getItem('authorizationToken')
+            }
+        }
+        )
+        .then(
+            response => {
+                console.log(response.data);
+            }
+        )
+        .catch(
+            error => {
+                console.log(error);
+            });
+    }
+
+    clickHandlerGift(gift_id) {
+        function fadeIn(target) {
+            target.classList.add('show');
+            target.classList.remove('hide')
+        }
+
+        this.setOpenDate(gift_id, {'open_date': Date.now()});
+        setTimeout(function ()
+        {
+            fadeIn(document.getElementById('gift_container'));
+            document.querySelectorAll(`div[data-gift-id="${gift_id}"]`)[0].classList.remove('hidden');
+        }, 500);
+    }
+
+    clickHandlerClose() {
+        function fadeOut(target) {
+            target.classList.add('hide');
+            target.classList.remove('show')
+        }
+
+        fadeOut(document.getElementById('gift_container'));
+        document.querySelectorAll('.gift_content:not(.hidden)')[0].classList.add('hidden');
     }
 
     getGifts() {
@@ -44,15 +91,13 @@ export default class Home extends Component {
 
     createGiftsList(giftsData) {
         try {
-            this.getGifts();
             return (
                 Object.keys(giftsData).map(
                 index =>
-                    <li key={index} className="list_item">
+                    <li key={index} className="list_item" onClick={this.clickHandlerGift.bind(null, giftsData[index]['id'])}>
                         <div className="container">
                             <div className="img">
                                 <img
-                                    data-gift-id={giftsData[index]['id']}
                                     src={giftsData[index]['open_date'] ? "../images/open_gift.svg" : "../images/gift.svg"}
                                     alt="gift"
                                 />
@@ -86,68 +131,7 @@ export default class Home extends Component {
 
     componentDidMount() {
         if(localStorage.getItem('isAuthorized') === 'false') this.props.history.push("/auth");
-
-        // function setOpenDate(gift_id, date_json) {
-        //     axios.put(
-        //     "/api/v1.0/" + gift_id + "/open",
-        //     JSON.stringify(date_json),
-        //     {
-        //         headers: {
-        //             'Content-Type': 'application/json',
-        //             'Authorization': localStorage.getItem('authorizationToken')
-        //         }
-        //     }
-        //     )
-        //     .then(
-        //         response => {
-        //             console.log(response.data);
-        //         }
-        //     )
-        //     .catch(
-        //         error => {
-        //             console.log(error);
-        //         });
-        // }
-
-        function fadeIn(target) {
-            target.classList.add('show');
-            target.classList.remove('hide')
-        }
-
-        function fadeOut(target) {
-            target.classList.add('hide');
-            target.classList.remove('show')
-        }
-
-        let gifts_elements = document.getElementsByClassName('list_item');
-        Array.from(gifts_elements).forEach(
-            element =>
-            {
-                element.addEventListener(
-                    'click',
-                    () =>
-                    {
-                        console.log(1);
-                        let gift_id = element.getElementsByTagName('img')[0].getAttribute('data-gift-id');
-
-                        //setOpenDate(gift_id, {'open_date': Date.now()});
-                        setTimeout(function ()
-                        {
-                            fadeIn(document.getElementById('gift_container'));
-                            document.querySelectorAll(`div[data-gift-id="${gift_id}"]`)[0].classList.remove('hidden');
-                        }, 500);
-                    });
-            }
-        );
-
-        document.getElementById('btn_close').addEventListener(
-            'click',
-            () =>
-            {
-                let gift_container = document.getElementById('gift_container');
-                fadeOut(gift_container);
-                document.querySelectorAll('.gift_content:not(.hidden)')[0].classList.add('hidden');
-            });
+        this.getGifts();
     }
 
     render() {
@@ -160,9 +144,9 @@ export default class Home extends Component {
                     <div id="gift_container" className="hide">
                         <div className="gift">
                             <header className="gift_header">
-                                <div id="btn_close"></div>
+                                <div id="btn_close" onClick={this.clickHandlerClose}></div>
                             </header>
-                            {this.createGiftsDetailed(this.state.gifts)}
+                            { this.createGiftsDetailed(this.state.gifts) }
                         </div>
                     </div>
                 </div>
